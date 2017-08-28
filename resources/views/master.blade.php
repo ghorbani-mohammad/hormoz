ed60df81b89a9361aac70b01fc071c20e63cb662
MGH <aali361@gmail.com>
2017-08-28 22:05:38 +0430

    Preparing Bootstrap Framework && IranSans Font && Master Layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style type="text/css">
+      @font-face {
+          font-family: iransans;
+          src: url(fonts/iransans.woff2);
+      }
+      body{
+        font-family: iransans;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>سلام</h1>
+    </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
